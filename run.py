d3fa5bf90fe4fb735476123dd47955ac8cd18649
mmmangooo <isabelle.angsas@gmail.com>
2021-01-27 11:49:33 +0000

    Change of port to 8080

--- run.py
+++ run.py
@@ -47,5 +47,5 @@ def careers():
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        port=int(os.environ.get("PORT", "8080")),
         debug=True)
